0ab42f5463d4057326961c292d9a673489fd2e23
AniekanabasiAbasiurua <aniekanabasi.abasiurua@gmail.com>
2024-12-26 23:27:35 -0500

    phase-1-tasklister-mini-project-complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.new_task_description.value)
+    form.reset()
+  })// your code here
 });
+
+function addToDo(todo) {
+  let temp = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = '-'
+  temp.textContent = `${todo}`
+  temp.appendChild(btn)
+  console.log(todo)
+  document.querySelector('#list').appendChild(temp)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove() 
+}
